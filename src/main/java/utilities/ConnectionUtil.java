70a0c80bf678781672a3eee07335b13af8d08bdf
arrokanna <103518328+arrokanna@users.noreply.github.com>
2022-05-27 14:35:36 -0500

    5/27 added get instence to connectionutil

--- src/main/java/utilities/ConnectionUtil.java
+++ src/main/java/utilities/ConnectionUtil.java
@@ -5,6 +5,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionUtil {
+
+    private static ConnectionUtil instance;
+
+    private ConnectionUtil() {}
+
+    public static ConnectionUtil getInstance() {
+        if (instance ==null) {
+            instance = new ConnectionUtil();
+        }
+
+        return instance;
+    }
+
     public static Connection getConnection(){
         String dbInfo = "jdbc:postgresql://localhost/postgres?user=postgres&password=password&currentSchema=foundation_project";
         try {
